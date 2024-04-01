5a27abaedbcb9d284f4e13d0ccbae1cb00eaf6fe
AllanGomes09 <115575030+AllanGomes09@users.noreply.github.com>
2024-04-01 20:44:17 -0300

    Update OrdemDeServicoRepository.java

--- servicex5/ordemDeServico/repositories/OrdemDeServicoRepository.java
+++ servicex5/ordemDeServico/repositories/OrdemDeServicoRepository.java
@@ -1,6 +1,6 @@
-package br.com.allan.servicex55.ordemDeServico.repositories;
+package br.com.allan.servicex5.ordemDeServico.repositories;
 
-import br.com.allan.servicex55.ordemDeServico.domain.OrdemDeServico;
+import br.com.allan.servicex5.ordemDeServico.domain.OrdemDeServico;
 import jakarta.persistence.Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
