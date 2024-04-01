645b13d09768a5ea9e1f92064a0bf7739fd14905
AllanGomes09 <115575030+AllanGomes09@users.noreply.github.com>
2024-04-01 20:45:18 -0300

    Update OrdemDeServicoService.java

--- servicex5/ordemDeServico/services/OrdemDeServicoService.java
+++ servicex5/ordemDeServico/services/OrdemDeServicoService.java
@@ -1,6 +1,6 @@
-package br.com.allan.servicex55.ordemDeServico.services;
+package br.com.allan.servicex5.ordemDeServico.services;
 
-import br.com.allan.servicex55.ordemDeServico.repositories.OrdemDeServicoRepository;
+import br.com.allan.servicex5.ordemDeServico.repositories.OrdemDeServicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
