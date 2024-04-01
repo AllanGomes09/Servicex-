aea2883b1ac71db4ff7ee13f293df48bd66c2211
AllanGomes09 <115575030+AllanGomes09@users.noreply.github.com>
2024-04-01 17:15:45 -0300

    Update CategoriaRepository.java

--- servicex5/categoria/repositories/CategoriaRepository.java
+++ servicex5/categoria/repositories/CategoriaRepository.java
@@ -1,6 +1,6 @@
-package br.com.allan.servicex55.categoria.repositories;
+package br.com.allan.servicex5.categoria.repositories;
 
-import br.com.alan.servicex55.categoria.domain.Categoria;
+import br.com.alan.servicex5.categoria.domain.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
